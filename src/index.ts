7e34ce7fdbfe647bc587f73d5330f3a13394bc2b
TOMMASO26175 <tommasoarioli2@hotmail.com>
2022-10-18 13:57:12 +0200

    code cleanup

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,15 @@ import { Decryptor } from "./Decryptor.js";
 import * as fs from 'fs';
 function main() {
     try {
-      var user_file = '../input_sensordatas.txt';
+      var user_file = '../input_output_files/input_sensordatas.txt';
       var r = readline.createInterface({
           input : fs.createReadStream(user_file)
       });
-      fs.truncate('../output_decryptedsensordata.txt', 0, err =>{if(err){return console.log(err)}})
+      fs.truncate('../input_output_files/output_decryptedsensordata.txt', 0, err =>{if(err){return console.log(err)}})
       r.on('line', function (text) {
           const decryptedSensor = new Decryptor(text)
           console.log("Deobfuscated Sensor:\n", decryptedSensor.deobfuscated_sensor_data);
-          fs.appendFile('../output_decryptedsensordata.txt',decryptedSensor.deobfuscated_sensor_data + "\n",err => {if(err){return console.log(err)}
+          fs.appendFile('../input_output_files/output_decryptedsensordata.txt',decryptedSensor.deobfuscated_sensor_data + "\n",err => {if(err){return console.log(err)}
           })
       });
     } catch (e) {
