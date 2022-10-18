7eeccbfa29bd9e0d2f63ef817e9fa03429c95b73
TOMMASO26175 <tommasoarioli2@hotmail.com>
2022-10-18 13:49:35 +0200

    input and output sensor data in txt

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,16 @@ import { Decryptor } from "./Decryptor.js";
 import * as fs from 'fs';
 function main() {
     try {
-      var user_file = '../sensordatas.txt';
+      var user_file = '../input_sensordatas.txt';
       var r = readline.createInterface({
           input : fs.createReadStream(user_file)
       });
+      fs.truncate('../output_decryptedsensordata.txt', 0, err =>{if(err){return console.log(err)}})
       r.on('line', function (text) {
           const decryptedSensor = new Decryptor(text)
           console.log("Deobfuscated Sensor:\n", decryptedSensor.deobfuscated_sensor_data);
+          fs.appendFile('../output_decryptedsensordata.txt',decryptedSensor.deobfuscated_sensor_data + "\n",err => {if(err){return console.log(err)}
+          })
       });
     } catch (e) {
       console.error(e);
